0ceffee069fb08a483ba6d77abff44ec4f5524eb
bowen951209 <bowen951209@gmail.com>
2024-09-21 08:09:24 +0800

    Add some comments.

--- src/main/java/net/bowen/draw/material/Metal.java
+++ src/main/java/net/bowen/draw/material/Metal.java
@@ -16,6 +16,10 @@ public class Metal extends Material{
 
     @Override
     public float getValue() {
+        // The fuzz value is stored in a strange form because I want to keep the memory arrangement in openGL simple.
+        // Every material is stored as a float. The integer digit is the id of the material, and for this metal material
+        // case, the floating point digits are the fuzz value. For example, metal material with fuzz value of 0.3 will
+        // be represented as 1.3, where 1 is the id of the metal material.
         return materialId + fuzz;
     }
 }
