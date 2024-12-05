1e5f5da1b7c501450cb7b90a23af5a7a57cb6a98
bowen951209 <bowen951209@gmail.com>
2024-12-05 18:48:12 +0800

    Add compiler error log line indicator debug utility. Fix #16.

--- src/main/java/net/bowen/system/Shader.java
+++ src/main/java/net/bowen/system/Shader.java
@@ -6,6 +6,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,15 +15,18 @@ import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL43.GL_COMPUTE_SHADER;
 
 public class Shader extends Deleteable {
+    private record ShaderSource(boolean isMainSource, String filename, String source) {
+    }
+
     private static final Pattern INCLUDE_PATTERN = Pattern.compile("#include\\s+<(.+?)>");
 
+    private final List<ShaderSource> sources = new ArrayList<>();
     private final int shaderId;
 
     // Constructor for vertex, fragment, or compute shaders
     public Shader(String resourcePath, int type) {
         super(false);
-        // Read the shader source from the file
-        String source = getSource(resourcePath);
+        String source = readSources(resourcePath);
 
         shaderId = glCreateShader(type);
         glShaderSource(shaderId, source);
@@ -29,10 +34,14 @@ public class Shader extends Deleteable {
 
         // Check for compilation errors
         if (glGetShaderi(shaderId, GL_COMPILE_STATUS) == GL_FALSE) {
-            throw new RuntimeException("Error creating shader: " + glGetShaderInfoLog(shaderId));
+            String glLog = glGetShaderInfoLog(shaderId);
+            String processedLog = splitFilesLog(glLog);
+            throw new RuntimeException(
+                    "Error creating shader. \n--- Log: ---\n" + processedLog + "\n--- GL Original Log: ---\n" + glLog
+            );
         }
 
-        System.out.println("Shader(" + shaderId + ")[" + getTypeName(type)+ "] created.");
+        System.out.println("Shader(" + shaderId + ")[" + getTypeName(type) + "] created.");
     }
 
     @Override
@@ -60,37 +69,81 @@ public class Shader extends Deleteable {
         }
     }
 
-    private static String getSource(String resourcePath) {
+    /**
+     * Convert the OpenGL compile log to the string that indicates what file and which line an error occurs.
+     */
+    private String splitFilesLog(String glLog) {
+        // Parse log lines with format `0(<line>) : `
+        Pattern logPattern = Pattern.compile("0\\((\\d+)\\) : ");
+        Matcher logMatcher = logPattern.matcher(glLog);
+
+        StringBuilder result = new StringBuilder();
+
+        int lastMatchEnd = 0;
+        while (logMatcher.find()) {
+            int globalLine = Integer.parseInt(logMatcher.group(1));
+            int localLine = 0;
+            String sourceFile = "";
+
+            int lineCount = 0;
+            int numLinesBefore = 0;
+            int numMainSourceLinesBefore = 0;
+            for (ShaderSource shaderSource : sources) {
+                int numLinesThisSource = (int) shaderSource.source.chars().filter(c -> c == '\n').count();
+                lineCount += numLinesThisSource;
+
+                if (lineCount >= globalLine) {
+                    sourceFile = shaderSource.filename;
+                    localLine = shaderSource.isMainSource ?
+                            globalLine - numLinesBefore + numMainSourceLinesBefore :
+                            globalLine - numLinesBefore;
+                    break;
+                }
+
+                numLinesBefore = lineCount;
+                if (shaderSource.isMainSource) numMainSourceLinesBefore += numLinesThisSource;
+            }
+
+            result.append(glLog, lastMatchEnd, logMatcher.start());
+            result.append("<").append(sourceFile).append(">(").append(localLine).append(") : ");
+
+            lastMatchEnd = logMatcher.end();
+        }
+        result.append(glLog, lastMatchEnd, glLog.length());
+
+        return result.toString();
+    }
+
+    private String readSources(String resourcePath) {
         // Read the raw source:
         String rawSource = readRaw(resourcePath);
 
         // Process source:
-        StringBuilder processedSource = new StringBuilder();
-        Matcher matcher = INCLUDE_PATTERN.matcher(rawSource);
+        Matcher includeMatcher = INCLUDE_PATTERN.matcher(rawSource);
 
         int lastMatchEnd = 0;
         // Iterate over all matches of `#include` directives
-        while (matcher.find()) {
-            // Append the code before the current `#include` directive to the result
-            processedSource.append(rawSource, lastMatchEnd, matcher.start());
+        while (includeMatcher.find()) {
+            // Add the part of main source
+            sources.add(new ShaderSource(true, resourcePath, rawSource.substring(lastMatchEnd, includeMatcher.start())));
 
-            // Extract the path of the included file from the `#include` directive
-            String includePath = matcher.group(1);
-
-            // Read the included file
+            // Add include source
+            String includePath = includeMatcher.group(1);
             String includeContent = readRaw("shaders/" + includePath);
-
-            // Append the content of the included file to the result
-            processedSource.append(includeContent);
+            sources.add(new ShaderSource(false, includePath, includeContent));
 
             // Update the end position of the last match
-            lastMatchEnd = matcher.end();
+            lastMatchEnd = includeMatcher.end();
         }
 
-        // Append the remaining content of the file after the last `#include` directive
-        processedSource.append(rawSource.substring(lastMatchEnd));
+        // Add the remaining content of the main source
+        sources.add(new ShaderSource(true, resourcePath, rawSource.substring(lastMatchEnd)));
 
-        return processedSource.toString();
+        StringBuilder finalSource = new StringBuilder();
+        for (ShaderSource shaderSource : sources)
+            finalSource.append(shaderSource.source);
+
+        return finalSource.toString();
     }
 
     private static String readRaw(String resourcePath) {
